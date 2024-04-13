12a6e231402a9937c0451e22f29550112a10892b
darwintree <17946284+darwintree@users.noreply.github.com>
2024-04-13 20:32:12 +0800

    bump imas-tools to 0.1.3

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from imas_tools.recochoku import query_metadata_for
 app = FastAPI()
 
 @app.get("/recochoku/metadata")
-async def metadata(song_name: str):
-    return query_metadata_for(song_name)
+async def metadata(song_name: str, choice: int=0):
+    return query_metadata_for(song_name, choice)
