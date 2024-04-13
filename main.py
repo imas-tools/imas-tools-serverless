b751bd73a2fea331aef02729ede3d6ab6dab8603
darwintree <17946284+darwintree@users.noreply.github.com>
2024-04-13 19:37:19 +0800

    update dependency

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from time import time
 from fastapi import FastAPI, __version__
 
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.responses import HTMLResponse
 from imas_tools.recochoku import query_metadata_for
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 @app.get("/recochoku/metadata")
 async def metadata(song_name: str):
