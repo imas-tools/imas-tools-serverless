1fd8a42506ca1d8ac53ce8c9df19854590d941cc
darwintree <17946284+darwintree@users.noreply.github.com>
2024-04-19 00:12:40 +0800

    feat: add calendar interface

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI, __version__
 
 from imas_tools.recochoku import query_metadata_for
+from imas_tools.portal.calendar import fetch_articles_for_month, fetch_articles_for_today
 
 app = FastAPI()
 
 @app.get("/recochoku/metadata")
 async def metadata(song_name: str, choice: int=0):
     return query_metadata_for(song_name, choice)
+
+
+@app.get("/portal/calendar/month")
+async def month_articles(year: int=0, month: int=0, brands: list[str]=[], subcategories: list[str]=[]):
+    return fetch_articles_for_month(year, month, brands, subcategories) # type: ignore
+
+
+@app.get("/portal/calendar/today")
+async def today_articles(
+    brands: list[str] = [], subcategories: list[str] = []
+):
+    return fetch_articles_for_today(brands, subcategories) # type: ignore
--- test_main.py
+++ test_main.py
@@ -7,3 +7,15 @@ def test_metadata():
     res = client.get(f"/recochoku/metadata?song_name={song_name}")
     assert res.status_code == 200
     assert res.json()["title"] == song_name
+
+def test_month_articles():
+    client = TestClient(app)
+    res = client.get(f"/portal/calendar/month")
+    assert res.status_code == 200
+    assert len(res.json()["article_list"]) > 0
+
+def test_today_articles():
+    client = TestClient(app)
+    res = client.get(f"/portal/calendar/today")
+    assert res.status_code == 200
+    assert len(res.json()["article_list"]) > 0
