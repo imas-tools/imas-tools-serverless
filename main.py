30fa38129a630e1da78f3efedc331f426b385ddd
darwintree <17946284+darwintree@users.noreply.github.com>
2025-01-16 22:34:23 +0800

    fix: fastapi list param

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-from fastapi import FastAPI, __version__
+from typing import Optional, List
+
+from fastapi import FastAPI, Query
 
 from imas_tools.recochoku import query_metadata_for
+from imas_tools.portal.interfaces import BRAND_CODE, SUBCATEGORY_CODE
 from imas_tools.portal.article import fetch_schedule_for_month, fetch_schedule_for_today
 from imas_tools.story.gakuen_parser import parse_messages
 
 app = FastAPI()
 
+
 @app.get("/recochoku/metadata")
-async def metadata(song_name: str, choice: int=0):
+async def metadata(song_name: str, choice: int = 0):
     return query_metadata_for(song_name, choice)
 
 
 @app.get("/portal/schedule/month")
 async def month_schedule(
-    year: int = 0, month: int = 0, brands: list[str] = [], subcategories: list[str] = []
+    year: int = 0,
+    month: int = 0,
+    brands: Optional[List[BRAND_CODE]] = Query([]),
+    subcategories: Optional[List[SUBCATEGORY_CODE]] = Query([]),
 ):
     return fetch_schedule_for_month(year, month, brands, subcategories)  # type: ignore
 
 
 @app.get("/portal/schedule/today")
-async def today_schedule(brands: list[str] = [], subcategories: list[str] = []):
+async def today_schedule(
+    brands: Optional[List[BRAND_CODE]] = Query([]),
+    subcategories: Optional[List[SUBCATEGORY_CODE]] = Query([]),
+):
     return fetch_schedule_for_today(brands, subcategories)  # type: ignore
+
 
 from pydantic import BaseModel
 
--- test_main.py
+++ test_main.py
@@ -1,12 +1,14 @@
 from main import app
 from fastapi.testclient import TestClient
 
+
 def test_metadata():
     client = TestClient(app)
     song_name = "Sunshine See May(M@STER VERSION)"
     res = client.get(f"/recochoku/metadata?song_name={song_name}")
     assert res.status_code == 200
     assert res.json()["title"] == song_name
+
 
 def test_month_schedule():
     client = TestClient(app)
@@ -17,17 +19,32 @@ def test_month_schedule():
 
 def test_today_schedule():
     client = TestClient(app)
-    res = client.get(f"/portal/schedule/today")
+    res = client.get(
+        f"/portal/schedule/today"
+    )
     assert res.status_code == 200
     assert len(res.json()["article_list"]) > 0
 
+
+def test_today_schedule_zero():
+    client = TestClient(app)
+    res = client.get(
+        f"/portal/schedule/today",
+        params={
+            "brands": ["IDOLMASTER"],
+            "subcategories": ["CD"],
+        },
+    )
+    assert res.status_code == 200
+    assert len(res.json()["article_list"]) == 0
+
+
 story_txt = r"""[backgroundgroup backgrounds=[background id=schoolroom-00 src=env_3d_adv_schoolroom-00-00-noon]]"""
+
 
 def test_gakuen_parser():
     client = TestClient(app)
-    res = client.post(f"/story/gakuen_parser", json={
-        "story_txt": story_txt
-    })
+    res = client.post(f"/story/gakuen_parser", json={"story_txt": story_txt})
     assert res.status_code == 200
     print(res.json())
     assert res.json()[0]["__tag__"] == "backgroundgroup"
